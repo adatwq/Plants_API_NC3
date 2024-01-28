6e70db50206ea79d4c6e9cea4fb45e9e152ac540
Khawlah Khalid <khawlah1920@hotmail.com>
2024-01-28 11:16:23 +0300

    async functions

--- Sources/App/Controllers/PlantsController.swift
+++ Sources/App/Controllers/PlantsController.swift
@@ -19,41 +19,45 @@ struct PlantsController: RouteCollection{
     }
     
     //GET
-    func index (req: Request)throws-> EventLoopFuture<[Plant]>{
-        Plant.query(on: req.db).all()
+    func index (req: Request)async throws-> [Plant]{
+      try await Plant.query(on: req.db).all()
     }
     
     //GET by ID
-    func getPlantByID(req: Request) throws -> EventLoopFuture<Plant>{
-        return Plant.find(req.parameters.get("id"), on: req.db)
-            .unwrap(or: Abort(.notFound))
+    func getPlantByID(req: Request) async throws -> Plant{
+        let plant = try await Plant.find(req.parameters.get("id"), on: req.db)
+        guard let plant else {
+            throw Abort(.notFound)
+        }
+          return plant
     }
     
     //PST
-    func post (req: Request)throws-> EventLoopFuture<Plant>{
+    func post (req: Request)async throws-> Plant{
         let plant = try req.content.decode(Plant.self)
-        return plant.create(on: req.db).map{plant}
-        
+        try await plant.create(on: req.db)
+        return plant
     }
     
     //PUT
-    func update(req: Request)throws->EventLoopFuture<HTTPStatus>{
+    func update(req: Request)async throws-> Plant {
         let newPlant = try req.content.decode(Plant.self)
-        return Plant.find(newPlant.id, on: req.db)
-            .unwrap(or: Abort(.notFound))
-            .flatMap {
-                $0.name = newPlant.name
-                return $0.update(on: req.db).transform(to: .ok)
-            }
+        let plantToUpdate = try await Plant.find(newPlant.id, on: req.db)
+        guard let plantToUpdate else { throw Abort(.notFound) }
+        plantToUpdate.name = newPlant.name
+        try await plantToUpdate.save(on: req.db)
+        return plantToUpdate
     }
     
-    //DELETE
-    func delete(req: Request)throws->EventLoopFuture<HTTPStatus>{
-        return Plant.find(req.parameters.get("id"), on: req.db)
-            .unwrap(or: Abort(.notFound))
-            .flatMap {$0.delete(on: req.db)}
-            .transform(to: .ok)
+
+    func delete(req: Request)async throws-> Plant{
+        let plant = try await Plant.find(req.parameters.get("id"), on: req.db)
+        guard let plant else { throw Abort(.notFound) }
+        try await plant.delete(on: req.db)
+        return plant
+        
     }
+
 }
 
 
